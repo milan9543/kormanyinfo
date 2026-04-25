b658bdc6af94b88ea0c1ffc438a52d9fe897a4d0
Milán Horváth <milan9543@gmail.com>
2026-04-25 18:20:10 +0200

    add more a11y and seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hangnem.cc",
+  integrations: [sitemap({ filter: (page) => !page.includes("/og/") })],
   vite: {
     plugins: [tailwindcss()]
   }
